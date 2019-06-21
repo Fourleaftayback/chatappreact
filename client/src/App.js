4cc2a8b8b7a2cdb5f3641e423e4182b308a6284e
Jay Shong <jay0shong@gmail.com>
2019-06-21 15:03:33 -0400

    testing socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "localhost:5000",
+
+      ///
+      color: "white"
+      ///
+    };
+  }
+
+  // sending sockets
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit("change color", this.state.color); // change 'red' to this.state.color
+  };
+
+  ///
+
+  // adding the function
+  setColor = color => {
+    this.setState({ color });
+  };
+
+  ///
+
+  render() {
+    // testing for socket connections
+
+    const socket = socketIOClient(this.state.endpoint);
+    socket.on("change color", col => {
+      document.body.style.backgroundColor = col;
+    });
+
+    return (
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => this.send()}>Change Color</button>
+
+        <button id="blue" onClick={() => this.setColor("blue")}>
+          Blue
+        </button>
+        <button id="red" onClick={() => this.setColor("red")}>
+          Red
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const http = require('http');
+const http = require("http");
+var cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const busboy = require("connect-busboy");
 const busboyBodyParser = require("busboy-body-parser");
-const socketIO = require('socket.io');
+const socketIO = require("socket.io");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -13,6 +14,7 @@ if (process.env.NODE_ENV !== "production") {
 const port = process.env.PORT;
 const db = process.env.MLAB || "mongodb://localhost/test";
 const app = express();
+
 const server = http.createServer(app);
 const io = socketIO(server);
 
@@ -26,33 +28,48 @@ mongoose.connect(db, {
   useNewUrlParser: true
 });
 
-io.on("connection", socket => {
-  console.log("user connected");
-  socket.on("disconnected", () => {
-    console.log('user disconnected')
-  });
-})
+app.use(cors);
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 app.use(busboy());
 app.use(busboyBodyParser());
 
-
-
 app.use("/user", authApi);
 app.use("/reset", resetApi);
-app.use("/list", userListApi)
+app.use("/list", userListApi);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build", {
-    maxAge: "7d"
-  }));
+  app.use(
+    express.static("client/build", {
+      maxAge: "7d"
+    })
+  );
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
   });
 }
 
-app.listen(port, () => console.log(`server connected on ${port}`));
+io.on("connection", socket => {
+  console.log("New client connected");
+
+  // just like on the client side, we have a socket.on method that takes a callback function
+  socket.on("change color", color => {
+    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
+    // we make use of the socket.emit method again with the argument given to use from the callback function above
+    console.log("Color Changed to: ", color);
+    io.sockets.emit("change color", color);
+  });
+
+  // disconnect is fired when a client leaves the server
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+//app.listen(port, () => console.log(`server connected on ${port}`));
+server.listen(port, () => console.log(port));
