8cb753e6234be9383ce3eb57d72a9afc7bc6fd95
Jay Shong <jay0shong@gmail.com>
2019-08-12 14:39:28 -0400

    added helmet

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const http = require("http");
-//const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const busboy = require("connect-busboy");
 const busboyBodyParser = require("busboy-body-parser");
 const socketIo = require("socket.io");
+const compression = require("compression");
+const helmet = require("helmet");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -15,6 +16,7 @@ const port = process.env.PORT;
 const db = process.env.MLAB || "mongodb://localhost/test";
 
 const app = express();
+app.use(compression());
 
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -23,7 +25,6 @@ const authApi = require("./routes/auth");
 const resetApi = require("./routes/reset");
 const userListApi = require("./routes/userList");
 const messageApi = require("./routes/messages");
-
 require("./config/passportUser")(passport);
 
 mongoose.connect(db, {
@@ -31,7 +32,6 @@ mongoose.connect(db, {
 });
 
 //add helmet here
-//app.use(cors);
 
 app.use(
   bodyParser.urlencoded({
